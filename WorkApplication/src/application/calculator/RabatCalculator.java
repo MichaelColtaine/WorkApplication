89c8dbfb9295e61821bc9cd69c635efa3003271a
MichalTrojek <MichalTrojek1@gmail.com>
2018-03-25 22:38:47 +0200

    Add tests
    Learning to use tests

--- WorkApplication/src/application/calculator/RabatCalculator.java
+++ WorkApplication/src/application/calculator/RabatCalculator.java
@@ -6,7 +6,6 @@ public class RabatCalculator {
 	}
 
 	public String getResult(String priceAsString, String priceWithTaxesAsString) {
-
 		if (isLegitInput(priceAsString.trim()) && isLegitInput(priceWithTaxesAsString.trim())) {
 			Double price = changeComma(priceAsString);
 			Double priceWithTax = changeComma(priceWithTaxesAsString);
